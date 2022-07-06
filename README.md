75bab75ef8d3bd8367334bae3fb94f52e2bf4368
3neutronstar <minsoo.kang0918@gmail.com>
2022-07-06 21:15:15 +0900

    CIL-ZOO (need to fix bic)

--- README.md
+++ README.md
@@ -1,13 +1,47 @@
-# CIL-Zoo
-Class Incremental Learning
+# RelatedWork
 
-## Preparation
-- Requirements
-
-## Execution Command
-``` shell scripts
-    # iCaRL
-    python main.py train --train_mode icarl --model resnet32 --dataset cifar100 --batch_size 128 --lr 2 --lr_steps 49,63 --epochs 70 --task_size 5 --weight_decay 1e-5 --gamma 0.2 --memory_size 2000
-    # EEIL
-    python main.py train --train_mode eeil --model resnet32 --dataset cifar100 --batch_size 128 --lr 0.1 --gamma 0.1 --epochs 250 --lr_steps 100,150,200 --weight_decay 0.0002
+- How to Implement (iCaRL)
 ```
+    python main.py train --train-mode icarl --gpu-ids 0,1 --model resnet32
+```
+
+- How to Implement (EEIL)
+```
+    python main.py train --train-mode eeil --gpu-ids 2 --task-size 10 --model resnet32 --batch-size 128 --lr 0.1 --gamma 0.1 --epochs 250 --lr-steps 100,150,200 --weight-decay 0.0002
+```
+
+
+- How to Implement (EEIL+NI)
+```
+    python main.py train --train-mode eeil --gpu-ids 1 --task-size 10 --model resnet34 --batch-size 128 --lr 0.1 --gamma 0.1 --epochs 250 --lr-steps 100,150,200 --weight-decay 0.0002 --natural-inversion True --inversion_epochs 2000
+``` 
+
+- How to Implement (BiC)
+```
+    python main.py train --train-mode bic --gpu-ids 2 --task-size 10 --model resnet32 --batch-size 128 --lr 0.1 --gamma 0.1 --epochs 250 --lr-steps 100,150,200 --weight-decay 0.0002
+```
+
+- 5 task
+
+| 5 task |    1 |     2 |     3 |     4 |     5 |
+|--------|-----:|------:|------:|------:|------:|
+| iCaRL  |      |       |       |       |       |
+| EEIL   | 82.5 | 72.12 | 65.75 | 59.08 | 54.23 |
+| BiC    |      |       |       |       |       |
+
+- 10 task
+
+| 10 task |  1 |    2 |    3 |    4 |     5 | 6     | 7     | 8     | 9     | 10    |
+|---------|---:|-----:|-----:|-----:|------:|-------|-------|-------|-------|-------|
+| iCaRL   |    |      |      |      |       |       |       |       |       |       |
+| EEIL    | 87 | 77.7 | 73.7 | 66.5 | 61.66 | 58.37 | 55.27 | 51.21 | 48.59 | 45.47 |
+| BiC     |    |      |      |      |       |       |       |       |       |       |
+|         |    |      |      |      |       |       |       |       |       |       |
+
+- 20 task
+
+| 20 task |    1 |    2 |    3 |     4 |     5 | 6    | 7     | 8    | 9     | 10   | 11    | 12    | 13    | 14    | 15    | 16    | 17    | 18    | 19    | 20    |
+|---------|-----:|-----:|-----:|------:|------:|------|-------|------|-------|------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|
+| iCaRL   |      |      |      |       |       |      |       |      |       |      |       |       |       |       |       |       |       |       |       |       |
+| EEIL    | 84.4 | 83.3 | 75.4 | 69.65 | 70.36 | 65.6 | 63.86 | 56.8 | 54.29 | 52.3 | 50.42 | 48.32 | 47.95 | 45.39 | 43.12 | 42.81 | 40.58 | 40.46 | 39.65 | 36.12 |
+| BiC     |      |      |      |       |       |      |       |      |       |      |       |       |       |       |       |       |       |       |       |       |
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 from implementor.baseline import Baseline
 from implementor.eeil import EEIL
 from implementor.icarl import ICARL
+from implementor.bic import BiC
 from model.basenet import get_model
 from implementor.evaluator.evaluation import Evaluation
 from utils.seed import fix_seed
@@ -27,11 +28,11 @@ def parse_args(args):
     parser.add_argument(
         '--device', type=str, default='cuda', help='choose NeuralNetwork')
     parser.add_argument(
-        '--batch_size', type=int, default=128, help='set mini-batch size')
+        '--batch-size', type=int, default=128, help='set mini-batch size')
     parser.add_argument(
-        '--num_workers', type=int, default=3, help='number of process you have')
+        '--num-workers', type=int, default=3, help='number of process you have')
     parser.add_argument(
-        '--weight_decay', type=float, default=1e-5, help='set optimizer\'s weight decay')
+        '--weight-decay', type=float, default=1e-5, help='set optimizer\'s weight decay')
     parser.add_argument(
         '--lr', type=float, default=2.0, help='set learning rate')
     parser.add_argument(
@@ -41,45 +42,62 @@ def parse_args(args):
     parser.add_argument(
         '--momentum', type=float, default=0.9, help='set momentum')
     parser.add_argument(
-        '--temperature', type=float, default=2.0, help='set temperature of knowledge distillation')
-    parser.add_argument(
         '--epochs', type=int, default=70, help='run epochs')
     parser.add_argument(
         '--dataset', type=str, default='cifar100', help='select dataset')
     parser.add_argument(
-        '--memory_size', type=int, default=2000, help='exemplar set size')
+        '--memory-size', type=int, default=2000, help='exemplar set size')
     parser.add_argument(
-        '--task_size', type=int, default=5, help='the number of task 5, 10 , 20, 25, 50, etc.')
-    parser.add_argument('--gpu_ids', default='0',
+        '--task-size', type=int, default=5, help='the number of task 5, 10 , 20, 25, 50, etc.')
+    parser.add_argument('--gpu-ids', default='0',
                         type=str, help=' ex) 0,1,2')
-    parser.add_argument('--detect_anomaly', default=False,
+    parser.add_argument('--detect-anomaly', default=False,
                         type=bool, help='Detect anomaly in PyTorch')
-    parser.add_argument('--lr_steps', help='lr decaying epoch determination', default=[49,63],
+    parser.add_argument('--lr-steps', help='lr decaying epoch determination', default=[49, 63],
                         type=lambda s: [int(item) for item in s.split(',')])
-    parser.add_argument('--dataset_path', help='dataset path (None: /data or .\\data\\dataset\\)', default=None)
-
+    parser.add_argument(
+        '--dataset-path', help='dataset path (None: /data or .\\data\\dataset\\)', default=None)
 
     if parser.parse_known_args(args)[0].mode.lower() == 'train':
         parser.add_argument(
-            '--train_mode', '-t', type=str, default='baseline', choices=['baseline','icarl','eeil'],
+            '--train-mode', '-t', type=str, default='baseline', choices=['baseline', 'icarl', 'eeil', 'bic'],
             help='Choose Train Mode')
+
+        if 'eeil' == parser.parse_known_args(args)[0].train_mode:
+            parser.add_argument(
+                '--temperature', type=float, default=2.0, help='set temperature of knowledge distillation')
+            parser.add_argument(
+                '--clip-grad', type=float, default=10000, help='clipping ratio')
+            parser.add_argument(
+                '--lamb', type=float, default=1.0, help='forgetting-intrasigence tradeoff')
+            parser.add_argument(
+                '--noise-grad', type=bool, default=False, help='add noise to gradients')
+
+        if 'bic' == parser.parse_known_args(args)[0].train_mode:
+            parser.add_argument(
+                '--temperature', type=float, default=2.0, help='set temperature of knowledge distillation')
+            parser.add_argument(
+                '--split_ratio', type=float, default=0.9, help='set split ratio of train validation split from old classes')
+            parser.add_argument(
+                '--clip-grad', type=float, default=10000, help='clipping ratio')
+            parser.add_argument(
+                '--lamb', type=float, default=1.0, help='forgetting-intrasigence tradeoff')
+            parser.add_argument(
+                '--noise-grad', type=bool, default=False, help='add noise to gradients')
+            parser.add_argument(
+                '--bias-correction-epochs', type=int, default=40, help='bias correction epochs')
 
     elif parser.parse_known_args(args)[0].mode.lower() == 'eval':
         parser.add_argument(
-            '--file_name', type=str, default=None,
+            '--file-name', type=str, default=None,
             help='Read file name')
         parser.add_argument('--evaluator', type=str, default='baseline',
                             choices=['baseline'])
 
     if 'tiny-imagenet' == parser.parse_known_args(args)[0].dataset:
-        parser.add_argument('--tiny_resize', type=bool,
+        parser.add_argument('--tiny-resize', type=bool,
                             default=False, help='choose 224 size or 64')
 
-    if parser.parse_known_args(args)[0].train_mode.lower()=='eeil':
-        eeil_aug=True
-    else:
-        eeil_aug=False
-    parser.add_argument('--eeil_aug',type=bool,default=eeil_aug,help="Apply EEIL Aug")
     return parser.parse_known_args(args)[0]
 
 
@@ -119,23 +137,23 @@ def main(args):
     parallel = configs['gpu_ids']
     if configs['mode'] != 'eval':
         configs.pop('gpu_ids')
-        dataset_path=configs.pop('dataset_path')
+        dataset_path = configs.pop('dataset_path')
         save_params(configs, current_path, time_data)
     else:
         if configs['file_name'] is None and 'pretrained' not in configs['model']:
             raise ValueError('Please provide file name')
         elif 'pretrained' not in configs['model']:
             file_name = configs['file_name']
-            seed=configs['seed']
+            seed = configs['seed']
             configs.update(load_params(
                 configs, current_path, configs['file_name']))
             configs['mode'] = 'eval'
             configs['file_name'] = file_name
             configs['device'] = device
-            configs['seed']=seed
-            time_data=file_name
-        dataset_path=configs['dataset_path']
-        
+            configs['seed'] = seed
+            time_data = file_name
+        dataset_path = configs['dataset_path']
+
     configs['gpu_ids'] = parallel
     print("="*30)
     print(configs)
@@ -157,7 +175,7 @@ def main(args):
         configs['num_classes'] = 196
     elif configs['dataset'] == 'dogs120':
         configs['num_classes'] = 120
-    elif configs['dataset'] in ['tiny-imagenet', 'cub200','imagenet200']:
+    elif configs['dataset'] in ['tiny-imagenet', 'cub200', 'imagenet200']:
         configs['num_classes'] = 200
     elif configs['dataset'] == 'inat2017':
         configs['num_classes'] = 5089
@@ -175,6 +193,7 @@ def main(args):
             'baseline': Baseline,
             'icarl': ICARL,
             'eeil': EEIL,
+            'bic': BiC,
         }
         learner = LEARNER[configs['train_mode']](
             model, time_data, save_path, device, configs)
